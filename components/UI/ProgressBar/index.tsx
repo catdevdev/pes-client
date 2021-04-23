31a09c3a77820cb33a44f30e2fb9bdb327361426
korov <korova2321@gmail.com>
2021-04-23 08:57:19 +0300

    Created progress bar component

---
+++ components/UI/ProgressBar/index.tsx
@@ -0,0 +1,16 @@
+/* imports */
+import c from "./index.module.scss";
+
+interface Props{
+  percentage: number;
+}
+
+const ProgressBar = (props: Props) => {
+  <div className={c.progressBar}>
+    <Filler percentage={props.percentage}/>
+  </div>
+}
+
+const Filler = (props: Props) => { 
+  return <div className={c.filler} style={{width: `${props.percentage}%`}}></div>
+}
