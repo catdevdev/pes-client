3367f824766cabd098dcc894652a17f3ee6f5a02
Vladislav Oliinyk <catdev.dev@gmail.com>
2023-02-09 18:10:34 +0200

    added

--- hooks/callWindows/index.tsx
+++ hooks/callWindows/index.tsx
@@ -122,7 +122,7 @@ export const useCallWindow = () => {
           },
           disableResize: true,
           title: {
-            label: 'Register new account in PES system',
+            label: 'Register new account in PES system 1',
           },
           ...data,
           body: {
