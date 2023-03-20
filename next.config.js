3b0980da3150de880bf82ec8c3a5a6c116088174
Vladislav Oliinyk <catdev.dev@gmail.com>
2023-03-20 23:02:54 +0200

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  // ...
+});
+
+module.exports = withPWA({
   typescript: {
     ignoreBuildErrors: true,
   },
-};
+});
---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>PES</title>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" href="/icon.svg" />
+          <meta name="theme-color" content="#fff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
